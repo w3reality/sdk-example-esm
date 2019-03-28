b44ec51bdd67a967c5b94b083fc0dbc71a3ebc65
Jay D <38098238+j-devel@users.noreply.github.com>
2019-03-28 13:15:37 +0700

    Update mymodule-esm.js

--- dist/mymodule-esm.js
+++ dist/mymodule-esm.js
@@ -4,7 +4,7 @@ const THREE = window.THREE;
 class MyModule {
     constructor() {
         const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({color: 0x008888});
+        const material = new THREE.MeshBasicMaterial({color: 0x00cccc});
         const cube = new THREE.Mesh(geometry, material);
         cube.position.set(0, 1, 7);
         const scene = new THREE.Scene();
@@ -18,7 +18,7 @@ class MyModule {
 
     updateScene() {
         const pos = this.cube.position;
-        if (pos.y > 4) {
+        if (pos.y > 3) {
             pos.y = 0;
         } else {
             pos.y += 0.1;
